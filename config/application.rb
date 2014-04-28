16eeb265452983c09406b7c301dbf4e6491c607e
Flaviano Honorato <flaviano.honorato@gmail.com>
2014-04-28 13:44:17 -0300

    first commit

---
+++ config/application.rb
@@ -0,0 +1,22 @@
+require File.expand_path('../boot', __FILE__)
+
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+Bundler.require(*Rails.groups)
+
+module Sisos
+  class Application < Rails::Application
+    config.filter_parameters += [:password]
+    config.time_zone = "Brasilia"
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :"pt-BR"
+    config.encoding = "utf-8"
+    config.active_record.whitelist_attributes = true
+  end
+end
