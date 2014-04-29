8831bd3226320a12d7b9c4c96ffccee73e9f94a8
flavianohonorato <flaviano.honorato@gmail.com>
2014-04-29 14:17:29 -0300

    Added controllers of clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,4 +1,56 @@
 class ClientsController < ApplicationController
-	def index
-	end
+  def index
+  	@clients = Client.all
+  	respond_to do |format|
+  	  format.html
+  	end
+  end
+
+  def show
+  	@client = Client.find(params[:id])
+  end
+
+  def new
+  	@client = Client.new
+  end
+
+  def create
+  	@client = Client.new(params[:client])
+
+  	if @client.save
+  	  flash[:notice] = 'Created with success!'
+  	  redirect_to @client
+  	else
+  	  flash[:error] = 'Not save, Try Again'
+  	  render :new
+  	end
+  end
+
+  def edit
+  	@client = Client.find(params[:id])
+  end
+
+  def destroy
+  	@client = Client.find(params[:id])
+  	  
+  	if client.destroy
+   	  flash[:notice] = 'Created with success!'
+      redirect_to @client
+  	else
+  	  flash[:error] = 'Not save, Try Again'
+  	  render :new
+  	end
+  end
+
+  def update
+  	@client = Client.find(params[:id])
+
+  	if @client.update_attributes(params[:client])
+  	  flash[:notice] = 'Deleted with success!'
+  	  redirect_to clients_path
+  	else
+  	  flash[:error] = 'Not Deleted, Try Again'
+  	end
+  	redirect_to clients_path
+  end
 end
