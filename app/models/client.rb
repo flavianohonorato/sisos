674f62f720273418d54717ff314a2563d1b6a52f
flavianohonorato <flaviano.honorato@gmail.com>
2014-06-10 10:40:48 -0300

    Updated model client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,4 @@
 class Client < ActiveRecord::Base
-	#attr_accessible :name, :email, :telefone, :endereco
-	#validates_presence_of :name, :telefone, :endereco
+	attr_accessible :name, :email, :phone, :birthday, :address
+	validates_presence_of :name, :phone
 end
